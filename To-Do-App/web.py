a56b119fce649282f00838dbcfb3d9db69bcef07
Regidan-codes <abhinadiger@gmail.com>
2023-11-20 20:33:30 +0530

    added streamlit

---
+++ To-Do-App/web.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import functions
+
+todos = functions.get_todos()
+
+st.title("To-Do App")
+st.subheader("This is my To-Do App")
+st.write("This app is to increase your productivity")
+
+for todo in todos:
+    st.checkbox(todo)
+
+st.text_input(label="", placeholder="Enter a new to-do...")
