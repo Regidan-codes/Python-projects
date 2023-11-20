5312a2e04adda192f593c6499341e3b6a2f1fe4c
Regidan-codes <abhinadiger@gmail.com>
2023-11-20 20:41:24 +0530

    added new add items feature

--- To-Do-App/web.py
+++ To-Do-App/web.py
@@ -3,6 +3,13 @@ import functions
 
 todos = functions.get_todos()
 
+
+def add_todo():
+    todo_local = st.session_state['new-todo'] + "\n"
+    todos.append(todo_local)
+    functions.write_todos(todos)
+
+
 st.title("To-Do App")
 st.subheader("This is my To-Do App")
 st.write("This app is to increase your productivity")
@@ -10,4 +17,5 @@ st.write("This app is to increase your productivity")
 for todo in todos:
     st.checkbox(todo)
 
-st.text_input(label="", placeholder="Enter a new to-do...")
+st.text_input(label="", placeholder="Enter a new to-do...",
+              on_change=add_todo, key="new-todo")
